3eba6d8754196afadc416634de0808ab95fd0c0f
Johan Lorenzo <jlorenzo@mozilla.com>
2016-06-23 13:59:41 +0200

    Test: Style fixes

--- test/api_reminders_test.js
+++ test/api_reminders_test.js
@@ -9,7 +9,9 @@ function getCurrentTimestampInSeconds() {
 
 function assertFullRemindersAreEqual(actual, expected,
   timestampBeforeCreation, timestampAfterCreation) {
-  expect(actual.created).within(timestampBeforeCreation, timestampAfterCreation);
+  expect(actual.created).within(
+    timestampBeforeCreation, timestampAfterCreation
+  );
 
   // Removed so we don't assert equality on this unknown value
   delete actual.created;
@@ -18,11 +20,6 @@ function assertFullRemindersAreEqual(actual, expected,
 
 describe('/reminders', function() {
   const remindersUrl = `${config.apiRoot}/reminders`;
-  const defaultReminder = {
-    recipient: 'John',
-    message: 'Pick up kids at school',
-    due: 1466613000,
-  };
 
   before(function() {
     return chakram.post(
@@ -49,44 +46,54 @@ describe('/reminders', function() {
   });
 
   it('should add a new reminder and make it visible', function() {
-    const updatedReminder = Object.assign({}, defaultReminder);
-    updatedReminder.recipient = 'Jane';
     const expectedLocation = `${remindersUrl}/1`;
-    const expectedReminder = {
-      due: 1466613000,
-      family: 'family_name',
-      id: 1,
+    const initialReminder = {
+      recipient: 'Jane',
       message: 'Pick up kids at school',
-      recipient: 'John',
+      due: Date.now() + 2 * 60 * 60,
     };
+    const expectedReminder = Object.assign(
+      { id: 1, family: 'family_name' },
+      initialReminder
+    );
+    const updatedReminder = {
+      recipient: 'John',
+      message: 'Go shopping',
+      due: Date.now() + 4 * 60 * 60,
+    };
+    const expectedUpdatedReminder = Object.assign(
+      {}, expectedReminder, updatedReminder
+    );
 
 
     let timestampAfterCreation;
     const timestampBeforeCreation = getCurrentTimestampInSeconds();
 
-    return chakram.post(remindersUrl, defaultReminder).then(res => {
+    return chakram.post(remindersUrl, initialReminder).then(res => {
       timestampAfterCreation = getCurrentTimestampInSeconds();
       expect(res).status(201);
       expect(res.response.headers.location).deep.equal('/api/v1/reminders/1');
+
       return chakram.get(expectedLocation);
     }).then(res => {
       expect(res).status(200);
       assertFullRemindersAreEqual(res.body, expectedReminder,
         timestampBeforeCreation, timestampAfterCreation);
+
       return chakram.put(expectedLocation, updatedReminder);
     }).then(res => {
       expect(res).status(204);
+
       return chakram.get(expectedLocation);
     }).then(res => {
-      const updatedExpectedReminder = Object.assign({}, expectedReminder);
-      updatedExpectedReminder.recipient = 'Jane';
-
       expect(res).status(200);
-      assertFullRemindersAreEqual(res.body, updatedExpectedReminder,
+      assertFullRemindersAreEqual(res.body, expectedUpdatedReminder,
         timestampBeforeCreation, timestampAfterCreation);
+
       return chakram.delete(expectedLocation);
     }).then(res => {
       expect(res).status(204);
+
       return chakram.get(remindersUrl);
     }).then(res => {
       expect(res.body).deep.equal([]);
