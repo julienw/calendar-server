eaa415cc307783a0c06be8a01a5e558c013aad8c
Johan Lorenzo <jlorenzo@mozilla.com>
2016-06-24 15:36:34 +0200

    Move /reminders routes to a separate file

--- app.js
+++ app.js
@@ -5,11 +5,9 @@ const cors = require('cors');
 
 const jwt = require('express-jwt');
 
-const debug = require('debug')('calendar-server:app');
-
-const reminders = require('./reminders');
 const login = require('./login');
 const config = require('./config');
+const addRemindersRoutes = require('./routes/reminders');
 
 require('./database').init(config.profile);
 
@@ -31,36 +29,7 @@ app.post(`${API_ROOT}/login`, login);
 // redirect if we're not logged in
 app.use(jwt({ secret: 'some secret that you should configure' }));
 
-app.route(`${API_ROOT}/reminders`)
-  .get((req, res, next) => {
-    reminders.index(req.user.family, req.query.start, req.query.limit)
-      .then(rows => res.send(rows))
-      .catch(next);
-  })
-  .post((req, res, next) => {
-    reminders.create(req.user.family, req.body).then((id) => {
-      debug('reminder with ID %s has been created in database', id);
-      res.status(201).location(`${API_ROOT}/reminders/${id}`).end();
-    }).catch(next);
-  });
-
-app.route(`${API_ROOT}/reminders/:reminder`)
-  .get((req, res, next) => {
-    reminders.show(req.user.family, req.params.reminder).then((reminder) => {
-      debug('found reminder %o', reminder);
-      res.send(reminder);
-    }).catch(next);
-  })
-  .delete((req, res, next) => {
-    reminders.delete(req.user.family, req.params.reminder)
-      .then(() => res.status(204).end())
-      .catch(next);
-  })
-  .put((req, res, next) => {
-    reminders.update(req.user.family, req.params.reminder, req.body)
-      .then(() => res.status(204).end())
-      .catch(next);
-  });
+addRemindersRoutes(app, API_ROOT);
 
 app.get('/', (req, res) => {
   res.send('You may want to use the API instead.');
---
+++ routes/reminders.js
@@ -0,0 +1,38 @@
+const debug = require('debug')('calendar-server:routes/reminders');
+
+const reminders = require('../reminders');
+
+function addRoutes(app, apiRoot) {
+  app.route(`${apiRoot}/reminders`)
+    .get((req, res, next) => {
+      reminders.index(req.user.family, req.query.start, req.query.limit)
+        .then(rows => res.send(rows))
+        .catch(next);
+    })
+    .post((req, res, next) => {
+      reminders.create(req.user.family, req.body).then((id) => {
+        debug('reminder with ID %s has been created in database', id);
+        res.status(201).location(`${apiRoot}/reminders/${id}`).end();
+      }).catch(next);
+    });
+
+  app.route(`${apiRoot}/reminders/:reminder`)
+    .get((req, res, next) => {
+      reminders.show(req.user.family, req.params.reminder).then((reminder) => {
+        debug('found reminder %o', reminder);
+        res.send(reminder);
+      }).catch(next);
+    })
+    .delete((req, res, next) => {
+      reminders.delete(req.user.family, req.params.reminder)
+        .then(() => res.status(204).end())
+        .catch(next);
+    })
+    .put((req, res, next) => {
+      reminders.update(req.user.family, req.params.reminder, req.body)
+        .then(() => res.status(204).end())
+        .catch(next);
+    });
+}
+
+module.exports = addRoutes;
