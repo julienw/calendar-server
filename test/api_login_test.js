24ce16207475dc010c9d06e72811f8fe3fbb1414
Julien Wajsberg <felash@gmail.com>
2016-06-24 17:41:51 +0200

    Use generator coroutines to better handle asynchronous tests

--- test/api_login_test.js
+++ test/api_login_test.js
@@ -6,28 +6,27 @@ const config = require('./config.js');
 
 describe('/login', function() {
 
-  before(function() {
-    return serverManager.start();
+  before(function* () {
+    yield serverManager.start();
   });
 
-  after(function() {
-    return serverManager.stop();
+  after(function* () {
+    yield serverManager.stop();
   });
 
-  it('can login', function() {
-    return chakram.post(
+  it('can login', function*() {
+    const res = yield chakram.post(
       `${config.apiRoot}/login`,
       { user: 'family_name', password: 'password' }
-    ).then(res => {
-      expect(res).status(200);
-      expect(res).schema({
-        type: 'object',
-        properties: {
-          token: {
-            type: 'string'
-          }
+    );
+    expect(res).status(200);
+    expect(res).schema({
+      type: 'object',
+      properties: {
+        token: {
+          type: 'string'
         }
-      });
+      }
     });
   });
 });
