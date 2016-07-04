3377792ad14cc79811f797513a77482ff31dcdec
Julien Wajsberg <felash@gmail.com>
2016-07-04 15:25:58 +0200

    fixed nits

--- scripts/populate_db.js
+++ scripts/populate_db.js
@@ -3,39 +3,59 @@
 const request = require('request-promise');
 
 
-const options = {
-  json: true
-};
 const baseUrl = process.env.TARGET_ENV === 'production'
   ? 'https://calendar.knilxof.org/api/v1'
   : 'http://localhost:3000/api/v1';
 
+const baseOptions = {
+  json: true,
+  url: `${baseUrl}/reminders`
+};
+
 const familyName = process.env.FAMILY || 'mozilla';
 
 function login() {
+  console.log('Logging in with user %s', familyName);
   return request.post({
     url: `${baseUrl}/login`,
     json: true,
     body: { user: familyName, password: 'password' }
   }).then(res => {
-    options.headers = {
+    console.log('... authenticated with token %s', res.token);
+    baseOptions.headers = {
       Authorization: `Bearer ${res.token}`
     };
   });
 }
 
 function insertReminders() {
-  const reminders = require('./reminders.json');
-  options.url = `${baseUrl}/reminders`;
+  let reminders = require('./reminders.json');
 
-  const promises = reminders.map(reminder => {
-    options.body = reminder;
-    return request.post(options);
+  const now = Date.now();
+  reminders = reminders.map((reminder, i) => {
+    reminder.due = now + i * 5 * 60 * 1000;
+    return reminder;
   });
 
-  return Promise.all(promises);
+  function postReminder(i) {
+    const reminder = reminders[i];
+    console.log('Inserting reminder %j', reminder);
+    const options = Object.assign({}, baseOptions, { body: reminder });
+    return request.post(options);
+  }
+
+  function postAndContinue(i) {
+    const promise = postReminder(i);
+    if (i === reminders.length - 1) {
+      return promise;
+    }
+
+    return promise.then(() => postAndContinue(i + 1));
+  }
+
+  return postAndContinue(0);
 }
 
-login().then(() =>
-  insertReminders()
-);
+login().then(insertReminders).then(() => {
+  console.log('Insertion is finished.');
+});
