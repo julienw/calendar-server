503ed0b3665fec078d2f8451c538e77d8cd955e7
Julien Wajsberg <felash@gmail.com>
2016-10-05 18:25:54 +0200

    Adds a test to check if reminders are sent to the right subscriptions (Closes #40)

--- test/notifications_test.js
+++ test/notifications_test.js
@@ -25,46 +25,65 @@ function waitForMqMessage() {
 describe('notifications', function() {
   this.timeout(10000); // Tests last longer than the regular 2s-allowed-span.
 
-  const subscription = {
-    title: 'my_beautiful_subscription',
-    subscription: {
-      endpoint: 'https://some.endpoint',
-      keys: {
-        p256dh: 'some_base_64',
-        auth: 'some_base_64'
-      }
-    }
-  };
-
-  const expectedSubscription = {
-    id: 1,
-    userId: 1,
-    title: subscription.title,
-    subscription: {
-      endpoint: subscription.subscription.endpoint,
-      keys: {
-        p256dh: subscription.subscription.keys.p256dh,
-        auth: subscription.subscription.keys.auth
-      },
+  const users = [
+    {
+      email: 'john@helloworld.com',
+      password: 'Hello World',
+      forename: 'John',
     },
-  };
+    {
+      email: 'Julien@julien.com',
+      password: 'Hello World',
+      forename: 'Julien',
+    },
+    {
+      email: 'johan@johan.com',
+      password: 'Hello France',
+      forename: 'Johan',
+    }
+  ];
 
-  const user = {
-    email: 'john@helloworld.com',
-    password: 'Hello World',
-    forename: 'John',
-  };
+  const subscriptions = [];
+  const expectedSubscriptions = [];
+  users.forEach((user, i) => {
+    const id = i + 1;
+    const subscription = {
+      title: `subscription_user_${id}`,
+      subscription: {
+        endpoint: `https://endpoint/user/${id}`,
+        keys: {
+          p256dh: 'some_base_64',
+          auth: 'some_base_64'
+        }
+      }
+    };
+
+    subscriptions.push(subscription);
+
+    expectedSubscriptions.push({
+      id,
+      userId: id,
+      title: subscription.title,
+      subscription: {
+        endpoint: subscription.subscription.endpoint,
+        keys: {
+          p256dh: subscription.subscription.keys.p256dh,
+          auth: subscription.subscription.keys.auth
+        },
+      },
+    });
+  });
 
   const inputs = [{
     recipients: [{ userId: 1 }],
     action: 'Pick up kids at school',
     due: Date.now(),
   }, {
-    recipients: [{ userId: 1 }],
+    recipients: [{ userId: 2 }],
     action: 'Buy milk',
     due: Date.now(),
   }, {
-    recipients: [{ userId: 1 }],
+    recipients: [{ userId: 3 }],
     action: 'Go to school',
     due: Date.now(),
   }];
@@ -81,7 +100,7 @@ describe('notifications', function() {
 
     return {
       reminder,
-      subscription: expectedSubscription
+      subscription: expectedSubscriptions[i]
     };
   });
 
@@ -89,8 +108,13 @@ describe('notifications', function() {
 
   beforeEach(function*() {
     yield serverManager.start();
-    user.id = yield* api.createUser(user);
-    yield* api.login(user.email, user.password);
+    for (const user of users) {
+      user.id = yield* api.createUser(user);
+    }
+    yield* api.login(users[0].email, users[0].password);
+    const groupId = yield* api.createGroup({ name: 'CD_Staff' });
+    yield* api.addUserToGroup(2, groupId);
+    yield* api.addUserToGroup(3, groupId);
   });
 
   afterEach(function* () {
@@ -121,14 +145,18 @@ describe('notifications', function() {
   describe('once subscriptions are registered', function() {
     beforeEach(function*() {
       mq.connect(mqSocket);
-      yield* api.createSubscription(subscription);
+      for (let i = 0; i < users.length; i++) {
+        yield* api.login(users[i].email, users[i].password);
+        yield* api.createSubscription(subscriptions[i]);
+      }
     });
 
     afterEach(function() {
       mq.disconnect(mqSocket);
     });
 
-    it('does not send a reminder twice to the message queue', function*() {
+    it('sends reminders to the right person and ' +
+       'does not send a reminder twice to the message queue', function*() {
       // We try to determine if the first reminder has been sent only once.
       // In order to do so, we verify that this reminder is not sent a second
       // time in the second setInterval. As our SQL queries are not ordered, it
